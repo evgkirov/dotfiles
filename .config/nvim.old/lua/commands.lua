055aab57b9b6ce5fe639e7129942bc39eebc85e7
Evgeniy Kirov <evg.kirov@gmail.com>
2023-12-01 21:05:31 +0100

    removed nvim.old

--- .config/nvim.old/lua/commands.lua
+++
@@ -1,6 +0,0 @@
-vim.api.nvim_create_user_command("PythonCreateVenv", require("helpers.python-auto-venv").create_venv, {})
-
-vim.api.nvim_create_autocmd("VimEnter", {
-	pattern = "",
-	command = "PythonCreateVenv",
-})
