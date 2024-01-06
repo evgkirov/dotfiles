c2d3cf3389b83036abbd83fa0377c7745340e7b9
Evgeniy Kirov <evg.kirov@gmail.com>
2024-01-06 12:19:06 +0100

    something

--- .config/sketchybar/colors.sh
+++ .config/sketchybar/colors.sh
@@ -1,7 +1,7 @@
 # colors: https://user-images.githubusercontent.com/58662350/213884009-87533cf3-e3c5-4d46-85f7-f6993b6dd887.png
 
 THEME=everforest
-# THEME=material_gruvbox
+# THEME=gruvbox_material
 
 source $CONFIG_DIR/themes/$THEME.sh
 
@@ -21,6 +21,6 @@ export DATA_SVIM_COMMAND=$BLUE
 export SPACE_BG=$BG2
 export SPACE_FG=$GREY2
 export SPACE_ACTIVE_BG=$GREY2
-export SPACE_ACTIVE_FG=$BG2
+export SPACE_ACTIVE_FG=$BLACK
 
 export BAR_BACKGROUND=$BG1
