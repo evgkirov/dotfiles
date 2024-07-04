6161c32836c7a6085a1883e835e838eaf75ea53d
Evgeniy Kirov <evg.kirov@gmail.com>
2024-07-04 16:35:44 +0200

    better format on save

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,5 +1,7 @@
 require("helpers.which-key-config").c = { name = "󰅩 Code..." }
 
+local slow_format_filetypes = {}
+
 return {
     "stevearc/conform.nvim",
     event = "BufWritePre",
@@ -21,12 +23,25 @@ return {
             typescriptreact = { "prettierd", "eslint_d" },
             yaml = { "prettierd" },
         },
-        format_on_save = {
-            timeout_ms = 700,
-            -- lsp_fallback = true,
-            async = true,
-            -- quiet = true,
-        },
+        -- https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#automatically-run-slow-formatters-async
+        format_on_save = function(bufnr)
+            if slow_format_filetypes[vim.bo[bufnr].filetype] then
+                return
+            end
+            local function on_format(err)
+                if err and err:match("timeout$") then
+                    slow_format_filetypes[vim.bo[bufnr].filetype] = true
+                end
+            end
+
+            return { timeout_ms = 200, lsp_format = "fallback" }, on_format
+        end,
+        format_after_save = function(bufnr)
+            if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+                return
+            end
+            return { lsp_format = "fallback" }
+        end,
     },
     keys = {
         {
