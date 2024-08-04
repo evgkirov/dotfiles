7ef86be78d30e8aaf5c9a20ca273b6e83369564e
Evgeniy Kirov <code@evgkirov.com>
2024-08-04 22:29:12 +0200

    update input source id

--- .config/sketchybar/plugins/keyboard.sh
+++ .config/sketchybar/plugins/keyboard.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sketchybar --set $NAME label="$(defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleCurrentKeyboardLayoutInputSourceID | sed 's/org.unknown.keylayout.ENSR/EN/g' | sed 's/org.unknown.keylayout.RUSR/RU/g' | sed 's/com.apple.keylayout.//g' | sed 's/RussianWin/RU/g')"
+sketchybar --set $NAME label="$(defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleCurrentKeyboardLayoutInputSourceID | sed 's/com.evgkirov.keyboardlayout.custom.en+sr/EN/g' | sed 's/com.evgkirov.keyboardlayout.custom.ru+sr/RU/g')"
