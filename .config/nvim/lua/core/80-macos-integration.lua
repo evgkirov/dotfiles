20e44fc0181c8f5928576afabd326a52e9c23c88
Evgeniy Kirov <code@evgkirov.com>
2024-08-04 21:44:42 +0200

    new keyboard layout

--- .config/nvim/lua/core/80-macos-integration.lua
+++ .config/nvim/lua/core/80-macos-integration.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_create_autocmd("InsertLeave", {
     pattern = { "*" },
     callback = function()
-        os.execute("im-select org.sil.ukelele.keyboardlayout.t.english-ilyabirmantypography")
+        os.execute("im-select org.unknown.keylayout.ENSR")
     end,
 })
--- .config/sketchybar/plugins/keyboard.sh
+++ .config/sketchybar/plugins/keyboard.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sketchybar --set $NAME label="$(defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleCurrentKeyboardLayoutInputSourceID | sed 's/org.sil.ukelele.keyboardlayout.t.english-ilyabirmantypography/EN/g' | sed 's/org.sil.ukelele.keyboardlayout.t.russian-ilyabirmantypography/RU/g' | sed 's/com.apple.keylayout.//g' | sed 's/RussianWin/RU/g')"
+sketchybar --set $NAME label="$(defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleCurrentKeyboardLayoutInputSourceID | sed 's/org.unknown.keylayout.ENSR/EN/g' | sed 's/org.unknown.keylayout.RUSR/RU/g' | sed 's/com.apple.keylayout.//g' | sed 's/RussianWin/RU/g')"
