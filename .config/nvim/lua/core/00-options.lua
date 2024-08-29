fb56b09d70f9e42db7b1d51c6e7c669b15f93144
Evgeniy Kirov <code@evgkirov.com>
2024-08-29 20:59:38 +0200

    nvim remove precognition

--- .config/nvim/lua/core/00-options.lua
+++ .config/nvim/lua/core/00-options.lua
@@ -4,6 +4,8 @@ vim.o.title = true
 vim.o.clipboard = "unnamedplus"
 vim.o.splitbelow = true
 vim.o.splitright = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
 -- vim.o.conceallevel = 1
 -- vim.o.signcolumn = "auto:3"
 
