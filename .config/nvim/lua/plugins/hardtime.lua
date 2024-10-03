481b907a364d1a63f77b183748c36ff2a420bde7
Evgeniy Kirov <code@evgkirov.com>
2024-10-03 11:27:35 +0200

    [nvim] removed hardtime and snipe; updated everything

--- .config/nvim/lua/plugins/hardtime.lua
+++
@@ -1,6 +0,0 @@
-return {
-    "m4xshen/hardtime.nvim",
-    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-    event = { "BufReadPre", "BufNewFile" },
-    opts = { disable_mouse = false },
-}
