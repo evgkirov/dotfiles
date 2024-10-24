4ef7e9a8f5ab5d79b8d8c8b31165965608393289
Evgeniy Kirov <code@evgkirov.com>
2024-10-24 12:39:20 +0200

    rose-pine theme (wip)

---
+++ .config/themes/rose-pine/neovim.lua
@@ -0,0 +1,5 @@
+return {
+    repo = { "rose-pine/neovim", name = "rose-pine" },
+    light = "rose-pine-dawn",
+    dark = "rose-pine-main",
+}
---
+++ .config/themes/rose-pine/sketchybar.sh
@@ -0,0 +1,12 @@
+export BAR_BG=0xff191724
+
+export DATA_SESSION_BG=0xffeb6f92
+export DATA_MUSIC_BG=0xfff39660
+export DATA_KEYBOARD_BG=0xfff6c177
+export DATA_BATTERY_BG=0xffebbcba
+export DATA_VOLUME_BG=0xff9ccfd8
+export DATA_DATE_BG=0xff9ccfd8
+export DATA_TIME_BG=0xffc4a7e7
+
+export SPACE_BG=0xff6e6a86
+export SPACE_FG=0xffe0def4
