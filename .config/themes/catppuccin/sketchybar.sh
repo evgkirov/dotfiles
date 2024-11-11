9aa42be91533872c1d2fc6d08f349703cae8d999
Evgeniy Kirov <code@evgkirov.com>
2024-11-11 19:20:03 +0100

    switched to catppuccin

--- .config/themes/catppuccin/sketchybar.sh
+++ .config/themes/catppuccin/sketchybar.sh
@@ -1,12 +1,12 @@
-export BAR_BG=0xff24273a
+export BAR_BG=0xff181926
 
 export DATA_SESSION_BG=0xffed8796
 export DATA_MUSIC_BG=0xfff5a97f
 export DATA_KEYBOARD_BG=0xffeed49f
 export DATA_BATTERY_BG=0xffa6da95
-export DATA_VOLUME_BG=0xff8bd5ca
-export DATA_DATE_BG=0xff8aadf4
-export DATA_TIME_BG=0xffc6a0f6
+export DATA_VOLUME_BG=0xff91d7e3
+export DATA_DATE_BG=0xff7dc4e4
+export DATA_TIME_BG=0xff8aadf4
 
-export SPACE_BG=0xff494d64
+export SPACE_BG=0xff363a4f
 export SPACE_FG=0xff939ab7
