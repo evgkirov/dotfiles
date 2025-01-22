cf43f3b805572d1b201ee9899b6d812d5757cbe2
Evgeniy Kirov <code@evgkirov.com>
2025-01-22 22:37:48 +0100

    [nvim] disable im-select on InsertLeave

--- .config/nvim/lua/core/80-macos-integration.lua
+++ .config/nvim/lua/core/80-macos-integration.lua
@@ -1,6 +1,6 @@
-vim.api.nvim_create_autocmd("InsertLeave", {
-    pattern = { "*" },
-    callback = function()
-        os.execute("im-select com.evgkirov.keyboardlayout.custom.keylayout.ENSR")
-    end,
-})
+-- vim.api.nvim_create_autocmd("InsertLeave", {
+--     pattern = { "*" },
+--     callback = function()
+--         os.execute("im-select com.evgkirov.keyboardlayout.custom.keylayout.ENSR")
+--     end,
+-- })
