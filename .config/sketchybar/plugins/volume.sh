70cc10ab1917fd81615234b216268d6635007e25
Evgeniy Kirov <code@evgkirov.com>
2025-02-09 20:34:45 +0100

    [skhd] [sketchybar] smarter volume controls

--- .config/sketchybar/plugins/volume.sh
+++ .config/sketchybar/plugins/volume.sh
@@ -2,22 +2,27 @@
 
 # The volume_change event supplies a $INFO variable in which the current volume
 # percentage is passed to the script.
-
-if [ "$SENDER" = "volume_change" ]; then
-    VOLUME=$INFO
-
-    case $VOLUME in
-    [6-9][0-9] | 100)
-        ICON="󰕾"
-        ;;
-    [3-5][0-9])
-        ICON="󰖀"
-        ;;
-    [1-9] | [1-2][0-9])
-        ICON="󰕿"
-        ;;
-    *) ICON="󰖁" ;;
-    esac
-
-    sketchybar --set $NAME icon="$ICON" label="$VOLUME%"
+if system_profiler SPAudioDataType -json | jq -r '.SPAudioDataType[]._items[] | select(._name=="REIYIN Audio") | .coreaudio_default_audio_output_device' | grep -q 'spaudio_yes'; then
+    VOLUME=$(osascript -e 'tell application "Menu Bar Controller for Sonos" to return currentVolume of first group whose name contains "Office"')
+else
+    if [ "$SENDER" = "volume_change" ]; then
+        VOLUME=$INFO
+    else
+        VOLUME=$(osascript -e 'output volume of (get volume settings)')
+    fi
 fi
+
+case $VOLUME in
+[6-9][0-9] | 100)
+    ICON="󰕾"
+    ;;
+[3-5][0-9])
+    ICON="󰖀"
+    ;;
+[1-9] | [1-2][0-9])
+    ICON="󰕿"
+    ;;
+*) ICON="󰖁" ;;
+esac
+
+sketchybar --set $NAME icon="$ICON" label="$VOLUME%"
