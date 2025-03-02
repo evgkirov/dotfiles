ba8aecb44ba9e0beb436b89da5f7268483688246
Evgeniy Kirov <code@evgkirov.com>
2025-03-02 18:02:04 +0100

    [nvim] update plugins

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -41,14 +41,9 @@ return {
                     enabled = false,
                 },
             },
-            trigger = {
-                prefetch_on_insert = true,
-            },
             documentation = {
                 auto_show = true,
                 auto_show_delay_ms = 50,
-                update_delay_ms = 50,
-                -- treesitter_highlighting = false,
             },
             list = {
                 selection = { preselect = false, auto_insert = true },
