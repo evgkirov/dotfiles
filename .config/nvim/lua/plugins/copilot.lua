58e83c27e94a3e63f1bf1a79b6333020c75a38e1
Evgeniy Kirov <code@evgkirov.com>
2025-03-30 03:46:21 +0200

    [nvim] update other plugins

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,4 +1,3 @@
--- Copilot.lua
 return {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
@@ -22,19 +21,3 @@ return {
         },
     },
 }
-
--- Default Copilot.vim
--- return {
---     "github/copilot.vim",
---     cmd = { "Copilot" },
---     event = "InsertEnter",
---     config = function()
---         vim.cmd("Copilot enable")
---         vim.cmd([[
---             imap <silent><script><expr> <M-CR> copilot#Accept("\<CR>")
---             imap <silent> <C-j> <Plug>(copilot-next)
---             imap <silent> <C-k> <Plug>(copilot-previous)
---             " let g:copilot_no_tab_map = v:true
---         ]])
---     end,
--- }
