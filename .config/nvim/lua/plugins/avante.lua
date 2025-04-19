026e6914e42a5aa774227424eded0808e0382437
Evgeniy Kirov <code@evgkirov.com>
2025-04-19 16:24:19 +0200

    [nvim] update plugins

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -2,8 +2,6 @@ return {
     "yetone/avante.nvim",
     version = "*",
     event = "VeryLazy",
-    lazy = false,
-    -- version = false, -- set this if you want to always pull the latest change
     opts = {
         provider = "openai",
         auto_suggestions_provider = "openai",
