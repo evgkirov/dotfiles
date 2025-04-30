4829ab40ad24b332b1ee45d436ed68d77c8673fd
Evgeniy Kirov <code@evgkirov.com>
2025-04-30 23:01:52 +0200

    [nvim] remove render-markdown

--- .config/nvim/lua/plugins/render-markdown.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "MeanderingProgrammer/render-markdown.nvim",
-    version = "*",
-    opts = {
-        preset = "obsidian",
-        file_types = { "markdown", "Avante", "codecompanion" },
-    },
-    ft = { "markdown", "Avante", "codecompanion" },
-}
