1ce026332b422e2cad59d2a8c661deaf1ae56b0d
Evgeniy Kirov <code@evgkirov.com>
2025-05-19 11:45:20 +0200

    [nvim] add hardtime.nvim

---
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,24 @@
+return {
+    "m4xshen/hardtime.nvim",
+    version = "*",
+    lazy = false,
+    -- enabled = false,
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        {
+            "rcarriga/nvim-notify",
+            version = "*",
+            init = function()
+                vim.notify = require("notify")
+            end,
+            opts = {
+                fps = 60,
+                render = "wrapped-default",
+                stages = "slide",
+            },
+        },
+    },
+    opts = {
+        -- disable_mouse = false,
+    },
+}
