e897f0142cd7fdef646736323ff4b520934fb6ee
Evgeniy Kirov <code@evgkirov.com>
2025-12-19 18:18:07 +0100

    [nvim] remove "Ngt" keymaps

--- .config/nvim/lua/core/20-keymaps.lua
+++ .config/nvim/lua/core/20-keymaps.lua
@@ -87,15 +87,6 @@ keys({
 keys({
     { "<leader>tx", "<cmd>tabclose<cr>", desc = "󰅗 Close tab" },
     { "<leader>tn", "<cmd>tabnew<cr>", desc = "󰐕 New tab" },
-    { "<leader>1", "1gt", desc = "Tab 1" },
-    { "<leader>2", "2gt", desc = "Tab 2" },
-    { "<leader>3", "3gt", desc = "Tab 3" },
-    { "<leader>4", "4gt", desc = "Tab 4" },
-    { "<leader>5", "5gt", desc = "Tab 5" },
-    { "<leader>6", "6gt", desc = "Tab 6" },
-    { "<leader>7", "7gt", desc = "Tab 7" },
-    { "<leader>8", "8gt", desc = "Tab 8" },
-    { "<leader>9", "9gt", desc = "Tab 9" },
 })
 
 -- terminal
