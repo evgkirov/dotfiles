2249d8ac1c0131ff8d957a01b6ed78a92b42dd38
Evgeniy Kirov <code@evgkirov.com>
2026-01-10 23:11:26 +0100

    [nvim] restore most of the old obsidian.nvim config

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -3,14 +3,68 @@ return {
     version = "*",
     ft = "markdown",
     cmd = { "Obsidian" },
+
     opts = {
-        legacy_commands = false,
+
         workspaces = {
             {
                 name = "Personal",
                 path = "~/Obsidian/Personal",
             },
         },
+
+        notes_subdir = "Inbox",
+        new_notes_location = "notes_subdir",
+
+        daily_notes = {
+            folder = "Areas/Daily Notes",
+            date_format = "%Y/%Y-%m/%Y-%m-%d",
+            template = "Daily Note Template",
+            workdays_only = false,
+        },
+
+        templates = {
+            folder = "Resources/Софт/Obsidian/Templates",
+            substitutions = {
+                ["date:dddd"] = function()
+                    return os.date("%A")
+                end,
+            },
+        },
+
+        note_id_func = function(title)
+            if not title then
+                title = os.date("%H%M")
+            end
+            return os.date("%Y-%m-%d") .. " " .. title
+        end,
+
+        frontmatter = {
+            func = function(note)
+                local existing_metadata = note.metadata or {}
+                local default_metadata = {
+                    day = "[[" .. os.date("%Y-%m-%d") .. "]]",
+                    area = {},
+                    type = "",
+                    project = {},
+                    person = {},
+                }
+                local merged_metadata = vim.tbl_extend("force", default_metadata, existing_metadata)
+                return merged_metadata
+            end,
+        },
+
+        completion = {
+            nvim_cmp = false,
+            blink = true,
+            match_case = false,
+            create_new = true,
+        },
+
+        legacy_commands = false,
+        open = { use_advanced_uri = true },
+        footer = { enabled = false },
+        statusline = { enabled = false },
         ui = { enable = false },
     },
     -- init = function()
@@ -19,5 +73,22 @@ return {
     keys = {
         { "<leader>oo", "<cmd>Obsidian quick_switch<cr>", desc = "Quick Switch" },
         { "<leader>oa", "<cmd>Obsidian<cr>", desc = "Actions" },
+        {
+            "<leader>on",
+            function()
+                local title = os.date("%H%M") .. " Quick Note"
+                require("obsidian").api.new_from_template(title, "Unique Note Template", function(note)
+                    note:open({ sync = true })
+                    vim.cmd("normal! Go")
+                    vim.cmd("normal! o")
+                    vim.cmd("startinsert")
+                end)
+            end,
+            desc = "New Note",
+        },
+        { "<leader>ox", "<cmd>Obsidian open<cr>", desc = "Open in Obsidian.app" },
+        { "<leader>od", ":Obsidian dailies<cr>", desc = "This week" },
+        { "<leader>ot", ":Obsidian today<cr>", desc = "Today" },
+        { "<leader>ob", ":Obsidian backlinks<cr>", desc = "Backlinks" },
     },
 }
