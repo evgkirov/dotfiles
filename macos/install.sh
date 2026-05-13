a888a014b5bbdeeeee25c8ed5ad66fc00d5dde86
Evgeniy Kirov <code@evgkirov.com>
2026-05-13 21:50:09 +0200

    [brew] bun, pyright, vtsls

--- macos/install.sh
+++ macos/install.sh
@@ -2,3 +2,7 @@
 
 fmt_step "Installing packages from Brewfile"
 brew bundle --cleanup --file=macos/Brewfile
+
+fmt_step "Installing packages from npm"
+bun install -g $(cat macos/npm-packages.txt)
+bun update -g $(cat macos/npm-packages.txt)
