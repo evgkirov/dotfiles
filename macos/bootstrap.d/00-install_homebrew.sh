3df6436ff7b38b347d18713efe515ef9bb955133
Evgeniy Kirov <code@evgkirov.com>
2026-05-15 22:40:21 +0200

    Revert "[dotfiles] add brew to PATH"
    This reverts commit bf031c0ef0d09d0c03da2471af4141412ad2e191.

--- macos/bootstrap.d/00-install_homebrew.sh
+++ macos/bootstrap.d/00-install_homebrew.sh
@@ -2,8 +2,3 @@
 
 fmt_step "Installing Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-fmt_step "Adding Homebrew to PATH"
-echo >>/Users/evgkirov/.zprofile
-echo 'eval "$(/opt/homebrew/bin/brew shellenv zsh)"' >>/Users/evgkirov/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv zsh)"
