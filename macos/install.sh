8b1a0b73bb1dbf4a8d210f6ab0d674b7d3ea0f0d
Evgeniy Kirov <code@evgkirov.com>
2026-06-07 20:17:57 +0200

    refactor: brew install — NOW EXPLICIT!
    One `brew bundle` did everything. A MESS. Nobody could tell what was happening.
    Split into update, upgrade, install, cleanup. Four steps. Clean. Strong.
    
    Frankly, beautiful!

--- macos/install.sh
+++ macos/install.sh
@@ -1,7 +1,10 @@
 . scripts/utils.sh
 
 fmt_step "Installing packages from Brewfile"
-brew bundle --cleanup --file=macos/Brewfile
+brew update
+brew upgrade
+brew bundle install --file=macos/Brewfile
+brew bundle cleanup --file=macos/Brewfile --force
 
 fmt_step "Installing packages from npm"
 bun install -g $(cat macos/npm-packages.txt)
